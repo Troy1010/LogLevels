fdd810dff7b29a9701e65ba1620152931a88389e
Troy <troyswanson1010@gmail.com>
2023-07-07 15:37:12 -0500

    pass in 1 string parameter

--- LogLevels/src/Commands.cpp
+++ LogLevels/src/Commands.cpp
@@ -7,8 +7,15 @@
 bool Cmd_PlaygroundLogLevels_Execute(COMMAND_ARGS)
 {
     logz(std::string(__func__) + "`Open")
+    
+    char	data[512];
+    if(ExtractArgs(PASS_EXTRACT_ARGS, &data))
+    {
+        logz(std::string("data:") + data)
+    }
+    
     logz(std::string(__func__) + "`Close")
     return true;
 }
 
-DEFINE_COMMAND_PLUGIN(PlaygroundLogLevels, "Playground command", 0, 0, nullptr)
+DEFINE_COMMAND_PLUGIN(PlaygroundLogLevels, "Playground command", 0, 1, kParams_OneString)
