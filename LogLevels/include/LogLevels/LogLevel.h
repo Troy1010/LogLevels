4fd8b53d6fdeb87579ef5ce30816cd8988c6a31c
Troy <troyswanson1010@gmail.com>
2023-07-07 17:05:22 -0500

    add LogLevel::FLOODING

--- LogLevels/include/LogLevels/LogLevel.h
+++ LogLevels/include/LogLevels/LogLevel.h
@@ -1,23 +1,24 @@
 #pragma once
 
 /**
-*   These are my favorite logging levels:
-*      logz
-*          Easy, temporary, local debugging. Should not be merged into releases.
-*      Info
-*          Important milestones that are always good to see, like user clicks, initialization complete, etc. Use sparingly.
-*      Error
-*          Something went wrong. Having this separate from "Info" is what allows it to have extra flare, like red highlighting and such in some IDEs.
-*      Warning
-*          Something might be wrong. Having this separate from "Info" is what allows it to have extra flare, like yellow highlighting and such in some IDEs.
-*      Debug
-*          Allows you to commit useful logs without having to see them unless you want to.
-*      Verbose
-*          For long, flooding messages.
+* These are my favorite logging levels:
+    logz
+        Easy, temporary, local debugging. Should not be merged into releases.
+    Info
+        Important milestones that are always good to see, like user clicks, initialization complete, etc. Use sparingly.
+    Error
+        Something went wrong. Having this separate from "Info" is what allows it to have extra flare, like red highlighting and such in some IDEs.
+    Warning
+        Something might be wrong. Having this separate from "Info" is what allows it to have extra flare, like yellow highlighting and such in some IDEs.
+    Debug
+        Allows you to commit useful logs without having to see them unless you want to.
+    Verbose
+        For long messages.
+    Flooding
+        For messages that might write to the log every frame.
 
-*   I generally like to keep the log level at ERROR, and then change it to DEBUG or VERBOSE if I realize that I want to see one of those logs, and don't mind filtering and/or scrolling to see.
+I generally like to keep the log level at ERROR, and freely use logz while developing. Then, before release, delete or change all logz to whatever makes sense. That way, I can see very accommodating logs while developing.. and if I come back later, I can see those old logs by setting the log level to DEBUG or VERBOSE.. but otherwise, they're hidden.
  */
-
 struct LogLevel
 {
     enum Enum
@@ -25,11 +26,12 @@ struct LogLevel
         ALWAYS,
         NONE,
         INFO,
-        ERRORZ,
         // Ends with "Z" because otherwise it's seen as the ERROR macro
+        ERRORZ,
         WARNING,
-        DEBUGZ,
         // Ends with "Z" because otherwise it's seen as the DEBUG macro
-        VERBOSE
+        DEBUGZ,
+        VERBOSE,
+        FLOODING
     };
 };
